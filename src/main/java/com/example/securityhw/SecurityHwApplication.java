05c0c82ea9532128196ec69a8f2f1817547d8aa8
Maxim Korchagin <Airfallsc2@gmail.com>
2024-07-21 16:38:36 +0300

    init

---
+++ src/main/java/com/example/securityhw/SecurityHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityHwApplication.class, args);
+    }
+
+}
